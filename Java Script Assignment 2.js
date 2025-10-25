7153905c9fa56c41b0ade47e8c88c0f6f648f339
Nitin Kumar <nitin2111kr@gmail.com>
2025-10-25 18:09:46 +0530

    Update Java Script Assignment 2.js

--- Java Script Assignment 2.js
+++ Java Script Assignment 2.js
@@ -8,17 +8,17 @@ marks = parseInt(marks);
 
 let grade = "";
 
-if(marks >= 90) {
+if(marks >= 90 && marks < 101) {
     grade = "A+";
-} else if(marks >= 80) {
+} else if(marks >= 80 && marks < 90) {
     grade = "A";
-} else if(marks >= 70) {
+} else if(marks >= 70 && marks < 80) {
     grade = "B+";
-} else if(marks >= 60) {
+} else if(marks >= 60 && marks < 70) {
     grade = "B";
-} else if(marks >= 50) {
+} else if(marks >= 50 && marks < 60) {
     grade = "C";
-} else if(marks >= 40) {
+} else if(marks >= 40 && marks < 50) {
     grade = "D";
 } else if(marks >= 33) {
     grade = "E"
